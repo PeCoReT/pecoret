399cfaf23b84d82097904ec73235d01f1d7f485e
blocki <62061776+blockisec@users.noreply.github.com>
2023-11-28 20:33:40 +0100

    fix tabmenu click

--- frontend/src/components/navigation/TabMenu.vue
+++ frontend/src/components/navigation/TabMenu.vue
@@ -13,15 +13,28 @@ export default {
         return {
             activeItem: null
         };
+    },
+    methods: {
+        onTabChange(event) {
+            this.activeItem = event.index;
+        }
+    },
+    watch: {
+        modelValue: {
+            deep: true,
+            handler(newValue) {
+                this.activeItem = newValue.findIndex((item) => this.$route.path === this.$router.resolve(item.route).path);
+            }
+        }
     }
 };
 </script>
 
 <template>
-    <TabMenu :model="modelValue" v-model:activeIndex="activeItem">
+    <TabMenu :model="modelValue" v-model:activeIndex="activeItem" @tab-change="onTabChange">
         <template #item="{ label, item, props }">
             <router-link v-if="item.route" v-slot="routerProps" :to="item.route" custom>
-                <a :href="routerProps.href" v-bind="props.action" @click="($event) => routerProps.navigate($event)" @keydown.enter.space="($event) => routerProps.navigate($event)">
+                <a :href="routerProps.href" v-bind="props.action" @click="($event) => routerProps.navigate($event)">
                     <span v-bind="props.icon" />
                     <span v-bind="props.label">{{ label }}</span>
                 </a>
