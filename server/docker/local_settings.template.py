4d17872e7f6d88fec25b31ee7657eb3a5caeb942
blocki <62061776+blockisec@users.noreply.github.com>
2024-03-02 16:20:27 +0100

    remove unused var

--- server/docker/local_settings.template.py
+++ server/docker/local_settings.template.py
@@ -4,9 +4,8 @@ import os
 # This is a minimal example settings file for PeCoReT docker
 # it is created, if no file exists to make the app running on first boot
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Required ettings:
+# Required settings:
 CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "http://localhost").split(",")
 CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', "http://localhost").split(",")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", 'backend').split(",")
