64c6d778411756ba13289febdefb90fa2e10d328
blocki <62061776+blockisec@users.noreply.github.com>
2024-10-04 11:20:24 +0200

    minor UI improvements

---
+++ frontend/src/components/badges/TechnologyBadge.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'TechnologyBadge',
+    props: {
+        technologyName: {
+            required: true
+        },
+        disableHover: {
+            default: false
+        }
+    },
+    computed: {
+        badgeClasses() {
+            return {
+                'bg-surface-950 text-white text-sm px-2 py-1 rounded border border-gray-800': true,
+                'hover:bg-surface-800 hover:cursor-pointer': !this.disableHover
+            };
+        }
+    }
+};
+</script>
+
+<template>
+    <span :class="badgeClasses">{{ technologyName }}</span>
+</template>
--- frontend/src/views/pages/attack_surface/URLDetail.vue
+++ frontend/src/views/pages/attack_surface/URLDetail.vue
@@ -2,10 +2,12 @@
 import ASMonitorService from '@/service/ASMonitorService';
 import TechnologyMultiSelectField from '@/components/forms/fields/TechnologyMultiSelectField.vue';
 import TagSelectField from '@/components/forms/fields/TagSelectField.vue';
+import TechnologyBadge from '@/components/badges/TechnologyBadge.vue';
+import TagBadgeButton from '@/components/badges/TagBadgeButton.vue';
 
 export default {
     name: 'ScanFindingDetail',
-    components: { TagSelectField, TechnologyMultiSelectField },
+    components: { TagBadgeButton, TechnologyBadge, TagSelectField, TechnologyMultiSelectField },
     data() {
         return {
             item: {},
@@ -48,13 +50,6 @@ export default {
             this.service.getURL(this.$api, this.urlId).then((response) => {
                 this.item = response.data;
             });
-        },
-        patchData(data) {
-
-            this.service.patchURL(this.$api, this.urlId, data).then((response) => {
-                this.item = response.data;
-                this.$toast.add({ severity: 'success', summary: 'Updated', detail: 'URL updated!', life: 3000 });
-            });
         }
     }
 };
@@ -68,17 +63,26 @@ export default {
             </div>
         </template>
         <div class="col-span-12 card">
-            <Form>
-                <Field label="URL">
-                    <InputText v-model="item.url" @update:model-value="patchData({ url: item.url })"></InputText>
-                </Field>
-                <Field label="Technologies">
-                    <TechnologyMultiSelectField v-model="item.technologies" @update:model-value="patchData({ technologies: item.technologies })"></TechnologyMultiSelectField>
-                </Field>
-                <Field label="Tags">
-                    <TagSelectField v-model="item.tags" display="chip" @update:model-value="patchData({ tags: item.tags })"></TagSelectField>
-                </Field>
-            </Form>
+            <h2 class="text-2xl font-semibold mb-2">URL: {{ item.url }}</h2>
+            <p class="mb-2"><strong>Status Code:</strong> {{ item.status_code }}</p>
+            <div id="technologies" v-if="item.technologies && item.technologies.length > 0">
+                <p class="mb-4"><strong>Technologies:</strong></p>
+                <div class="flex flex-wrap space-x-2 mb-4">
+                    <TechnologyBadge :technology-name="technology.name" v-for="technology in item.technologies" :key="technology.pk" :disableHover="true"></TechnologyBadge>
+                </div>
+            </div>
+
+            <div id="tags" v-if="item.tags && item.tags.length > 0">
+                <p class="mb-2"><strong>Tags:</strong></p>
+                <TagBadgeButton :label="tag" v-for="tag in item.tags" :key="tag.pk"></TagBadgeButton>
+            </div>
+
+            <div class="mt-3" v-if="item.response">
+                <h3 class="text-xl font-semibold mb-2">Response</h3>
+                <div class="card bg-surface-950">
+                    <code style="white-space: pre-wrap">{{ item.response.slice(0, 2000) }}</code>
+                </div>
+            </div>
         </div>
     </BaseLayout>
 </template>
